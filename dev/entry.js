1441e67f00ba676bdec1f81e1bdce6687c50ce18
akselthomsen <thomsenaksel@gmail.com>
2025-11-03 17:49:24 +0100

    fix: not check bundle.js

--- dev/entry.js
+++ dev/entry.js
@@ -4,13 +4,13 @@ const yaml = require('js-yaml')
 const ajv = new Ajv()
 
 // Define createValidator function that compiles AJV validator from schema string
-function createValidator(schemaString) {
+function createValidator (schemaString) {
   const schema = JSON.parse(schemaString)
   return ajv.compile(schema)
 }
 
 // Define function to validate YAML string using validator
-function validateYaml(validateFunc, yamlString) {
+function validateYaml (validateFunc, yamlString) {
   const data = yaml.load(yamlString)
   const valid = validateFunc(data)
   return {

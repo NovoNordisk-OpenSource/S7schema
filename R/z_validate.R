66c215b8c6d20d7b7584e5fb133cc6f0af221f0b
akselthomsen <thomsenaksel@gmail.com>
2025-11-03 16:28:59 +0100

    feat: exported valdiate function

--- R/z_validate.R
+++ R/z_validate.R
@@ -3,17 +3,45 @@
 #' Check if content is in accordance with a schema.
 #'
 #' @param x definition
-#' @param ... passed along to individual methods
+#' @param schema passed along to individual methods
 #' @export
-validate_schema <- S7::new_generic(
-  name = "validate_schema",
-  dispatch_args = "x"
+validate <- S7::new_generic(
+  name = "validate",
+  dispatch_args = "x",
+  fun = \(x, schema) {
+    S7::S7_dispatch()
+  }
 )
 
-#' @export
-S7::method(validate_schema, schema_validator) <- function(
-  x,
-  content
-) {
-  use_validator(validator = x, content = content)
+#' @noRd
+validate_list <- function(x, schema) {
+  use_validator(
+    validator = validator(schema = schema),
+    yaml_content = yaml::as.yaml(x)
+  )
+
+  invisible(x)
+}
+
+S7::method(validate, S7::class_list) <- function(x, schema) {
+  validate_list(x = x, schema = schema)
+}
+
+#' @noRd
+validate_yaml <- function(x, schema) {
+  content <- x |>
+    assert_file(ext = c("yml", "yaml")) |>
+    readLines(warn = FALSE) |>
+    paste(collapse = "\n")
+
+  use_validator(
+    validator = validator(schema = schema),
+    yaml_content = content
+  )
+
+  invisible(x)
+}
+
+S7::method(validate, S7::class_character) <- function(x, schema) {
+  validate_yaml(x = x, schema = schema)
 }
---
+++ tests/testthat/test-z_validate.R
@@ -0,0 +1,45 @@
+test_that("simple validation on lists works", {
+  validate(
+    x = list(id = "a"),
+    schema = test_path("schemas", "simple.json")
+  ) |>
+    expect_no_condition() |>
+    expect_equal(
+      expected = list(id = "a")
+    )
+
+  validate(
+    x = list(fake = "b"),
+    schema = test_path("schemas", "simple.json")
+  ) |>
+    expect_error(
+      "must NOT have additional properties.* additionalProperty: fake"
+    )
+
+  validate(
+    x = list(id = 1),
+    schema = test_path("schemas", "simple.json")
+  ) |>
+    expect_error(
+      "/id.* must be string"
+    )
+})
+
+test_that("simple valdiation of yaml files works", {
+  validate(
+    x = test_path("input", "simple.yml"),
+    schema = test_path("schemas", "simple.json")
+  ) |>
+    expect_no_condition() |>
+    expect_equal(
+      expected = test_path("input", "simple.yml")
+    )
+
+  validate(
+    x = test_path("input", "simple_error.yml"),
+    schema = test_path("schemas", "simple.json")
+  ) |>
+    expect_error(
+      "must NOT have additional properties.* additionalProperty: error"
+    )
+})

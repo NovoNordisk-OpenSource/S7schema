cfd801dbac51894218cd79d99d528a1b804aa4f7
akselthomsen <thomsenaksel@gmail.com>
2025-11-11 12:38:28 +0100

    feat: write generic

---
+++ R/z_write.R
@@ -0,0 +1,50 @@
+#' Write configuration file
+#'
+#' Thin wrapper around `yaml::write_yaml()` calling `validate()` before
+#' creating the file, ensuring that the saved configuration is valid.
+#'
+#' @param x `S7schema` object to write.
+#' @param file `character(1)` path to the file to write to.
+#' @param ... Additional arguments passed along to `yaml::write_yaml()`.
+#' @examples
+#' # Read configuration file:
+#' x <- S7schema(
+#'   file = system.file("examples/config.yml", package = "S7schema"),
+#'   schema = system.file("examples/schema.json", package = "S7schema")
+#' )
+#'
+#' print(x)
+#'
+#' # Edit content
+#' x$my_config_var <- 2
+#'
+#' # Save new file
+#' write_config(
+#'   x = x,
+#'   file = tempfile(fileext = ".yml")
+#' )
+#'
+#' @export
+write_config <- S7::new_generic(
+    name = "write_config",
+    dispatch_args = "x",
+    fun = \(x, file, ...) {
+        S7::S7_dispatch()
+    }
+)
+
+#' @noRd
+S7::method(write_config, S7schema) <- function(x, file, ...) {
+    write_valid_config(x, file, ...)
+}
+
+#' @noRd
+write_valid_config <- function(x, file, ...) {
+    validate(x)
+
+    yaml::write_yaml(
+        x = x,
+        file = file,
+        ...
+    )
+}
---
+++ tests/testthat/test-z_write.R
@@ -0,0 +1,23 @@
+test_that("writing works", {
+  x <- S7schema(
+    file = test_path("input", "simple.yml"),
+    schema = test_path("schemas", "simple.json")
+  )
+
+  tmpfile <- withr::local_tempfile(fileext = ".yml")
+
+  write_config(x = x, file = tmpfile) |>
+    expect_no_condition()
+
+  file.exists(tmpfile) |>
+    expect_true()
+
+  y <- S7schema(
+    file = tmpfile,
+    schema = test_path("schemas", "simple.json")
+  ) |>
+    expect_no_condition()
+
+  all.equal(x, y) |>
+    expect_true()
+})

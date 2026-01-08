1a3846bfa240d8d1ece769c2e02693c3621019fe
akselthomsen <thomsenaksel@gmail.com>
2026-01-08 17:09:05 +0100

    wip

---
+++ R/utils-document.R
@@ -0,0 +1,53 @@
+#' @noRd
+doc_asis <- function(x, ...) {
+  rlang::check_installed("knitr")
+  knitr::asis_output(x, ...)
+}
+
+#' @noRd
+doc_text <- function(txt) {
+  if (is.null(txt)) {
+    return(NULL)
+  }
+
+  as_character_1(txt)
+}
+
+#' @noRd
+doc_header <- function(txt, level = 2) {
+  rep(x = "#", times = level) |>
+    paste(collapse = "") |>
+    paste(txt) |>
+    doc_text()
+}
+
+#' @noRd
+doc_name_repair <- function(x) {
+  return(x)
+  x |>
+    stringr::str_replace_all("^\\$ref$", "reference") |>
+    stringr::str_to_title()
+}
+
+#' @noRd
+doc_kable <- function(x) {
+  withr::local_options(.new = list(knitr.kable.NA = ""))
+
+  x |>
+    knitr::kable() |>
+    as_character_1(collapse = "\n")
+}
+
+#' @noRd
+as_character_1 <- function(x, collapse = "<br>") {
+  x |>
+    as.character() |>
+    paste(collapse = collapse)
+}
+
+#' @noRd
+as_character_named <- function(x) {
+  x |>
+    as.character() |>
+    setNames(nm = doc_name_repair(names(x)))
+}
--- R/z_document.R
+++ R/z_document.R
@@ -1,23 +1,143 @@
 x <- jsonlite::read_json("../mighty.metadata/inst/schema/adam.json")
 
-str(x)
+#' @export
+document_schema <- function(x, header_level = 2) {
+  header_level <- 2
 
-x[["title"]]
-x[["description"]]
+  doc_header(txt = x[["title"]], level = header_level)
 
-# Object properties
-document_object(x[["properties"]]) |>
-  knitr::kable()
+  doc_text(txt = x[["description"]])
+}
 
-# Object not properties
-x[["definitions"]][["parameter"]][-3] |>
-  purrr::map(as_character_1) |>
-  as_character_named() |>
-  tibble::enframe()
+#' @noRd
+discard_entries <- function(
+  x,
+  discard = c(
+    "$schema",
+    "$id",
+    "title",
+    "description",
+    "properties",
+    "definitions"
+  )
+) {
+  i <- which(names(x) %in% discard)
+  x[i] <- NULL
+  x
+}
 
-# String
-x[["definitions"]][["cdisc"]][["class"]] |>
-  purrr::map(as_character_1) |>
-  as_character_named() |>
-  tibble::enframe() |>
-  doc_kable()
+#' @noRd
+document_entry <- function(x) {
+  entry_type <- x$type
+  if ("oneOf" %in% names(x)) {
+    entry_type <- "oneOf"
+  }
+
+  if (is.null(entry_type)) {
+    return(
+      x |>
+        discard_entries() |>
+        lapply(document_entry) |>
+        as_character_1(collapse = "\n\n")
+    )
+  }
+
+  switch(
+    EXPR = entry_type,
+    object = document_object(x),
+    oneOf = document_oneOf(x),
+    document_default(x)
+  )
+}
+
+#' @noRd
+document_default <- function(x) {
+  x |>
+    discard_entries() |>
+    purrr::map(as_character_1) |>
+    as_character_named() |>
+    tibble::enframe(name = "name") |>
+    tidyr::pivot_wider() |>
+    doc_kable()
+}
+
+#' @noRd
+document_object <- function(x) {
+  c(
+    document_default(x),
+    "",
+    document_object_properties(x$properties, x$required)
+  ) |>
+    as_character_1(collapse = "\n")
+}
+
+#' @noRd
+document_object_properties <- function(properties, required = NULL) {
+  if (is.null(properties)) {
+    return(NULL)
+  }
+
+  properties |>
+    tibble::enframe(name = "name") |>
+    tidyr::unnest_wider(
+      col = value,
+      names_repair = doc_name_repair
+    ) |>
+    dplyr::mutate(
+      requried = name %in% required
+    ) |>
+    doc_kable()
+}
+
+#' @noRd
+document_oneOf <- function(x) {
+  x[["oneOf"]] |>
+    purrr::map(
+      .f = \(x) {
+        x |>
+          discard_entries() |>
+          purrr::map(as_character_1) |>
+          as_character_named() |>
+          tibble::enframe(name = "name") |>
+          tidyr::pivot_wider()
+      }
+    ) |>
+    dplyr::bind_rows() |>
+    doc_kable()
+}
+
+# document_entry <- function(x) {
+#   x |>
+# purrr::map(as_character_1) |>
+#   as_character_named() |>
+#   tibble::enframe(name = "name") |>
+#   tidyr::pivot_wider()
+# }
+
+# document_properties <- function(x) {
+#
+#   properties <- x[["properties"]] |>
+#     tibble::enframe(name = "name") |>
+#     tidyr::unnest_wider(
+#       col = value,
+#       names_repair = doc_name_repair
+#     )
+
+#   required <- unlist(x[["required"]])
+#   if (!is.null(required)) {
+#     properties$Required <- character(length = nrow(properties))
+#     properties$Required[properties$Name %in% required] <- "Yes"
+#   }
+
+#   if ("Reference" %in% names(properties)) {
+#     i <- which(!is.na(properties[["Reference"]]))
+#     ref <- properties[["Reference"]][i]
+#     ref_text <- stringr::str_remove_all(ref, "^.*definitions/")
+#     ref_id <- stringr::str_remove(ref, "/") |>
+#       stringr::str_replace_all("/", "-")
+#     properties[["Type"]][i] <- paste0("[", ref_text, "](", ref_id, ")")
+#     properties[["Reference"]] <- NULL
+#   }
+
+#   properties
+# }

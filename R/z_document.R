282b826593729c1b838b96466cb3bab4ef4eb3a1
akselthomsen <thomsenaksel@gmail.com>
2026-01-26 10:50:27 +0100

    fix: documentation

--- R/z_document.R
+++ R/z_document.R
@@ -1,8 +1,25 @@
-#' Document schema
-#' @param x description
-#' @param header_start_level description
+#' Document configuration schema
+#' @description
+#' Creates markdown documentation of a configuration suitable for
+#' use in e.g. vignettes to provide easy readable documentation
+#' for users.
+#'
+#' @param x `character(1)` path to JSON schema, `list()` of already loaded specifications, or `S7schema()` object.
+#' @param header_start_level `numeric(1)` Level of initial header. All subheaders will continously he one level smaller.
+#' @return `character(1)`/`knitr::asis_output()` markdown with the documentation.
 #' @examples
+#' # Simple example schema
 #' system.file("examples/schema.json", package = "S7schema") |>
+#'   document_schema(2) |>
+#'   cat()
+#'
+#' # Changing header start level to 1
+#' system.file("examples/schema.json", package = "S7schema") |>
+#'   document_schema(1) |>
+#'   cat()
+#'
+#' # Example with definitions
+#' system.file("examples/definitions.json", package = "S7schema") |>
 #'   document_schema(2) |>
 #'   cat()
 #'

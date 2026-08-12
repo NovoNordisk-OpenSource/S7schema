d04c1f08baf8821cce480a64e419646e643711d2
akselthomsen <thomsenaksel@gmail.com>
2026-08-12 16:17:51 +0200

    fix: document class in the top of the script

--- R/y_S7schema.R
+++ R/y_S7schema.R
@@ -1,3 +1,51 @@
+#' Work with valid configurations
+#' @description
+#' `S7schema()` provides a generic way of working with yaml configuration files.
+#'
+#' The object is created by supplying both an initial YAML configuration (`file`)
+#' and the JSON schema definition (`schema`) of the configuration file.
+#'
+#' The initial configuration is validated before the new object is returned.
+#' If not valid the first error is thrown, together with a path to the entry in the YAML file,
+#' and a description of the error.
+#'
+#' The `S7schema` class inherits from `list`, ensuring that the content of the YAML
+#' file can be accessed as if read directly with `yaml::read_yaml()`, and supports
+#' the below workflow:
+
+#' 1. Read and validate config file: `x <- S7schema(...)`
+#' 2. Edit content as if it was a list: `x$new_entry <- "new_value"`
+#' 3. Validate new content against the original schema: `validate(x)`
+#' 4. Use values in downstream functions: `x$new_entry`
+#'
+#' @details
+#' See internal [validator()] documentation for more info on how the validation is done.
+#'
+#' @param file `character(1)` path to a yaml file to be checked.
+#' @param schema `character(1)` path to a JSON schema.
+#' @param .data `list()` input to create the object. Mutually exclusive with `file`.
+#' @section Properties:
+#' \describe{
+#'   \item{schema}{`character(1)` path to JSON schema being used to validate against.}
+#'   \item{validator}{Internal [validator()] used to validate the content (read-only).}
+#'   \item{file}{`character(1)` path to the source YAML file.}
+#' }
+#' @returns New `S7schema` object.
+#' @examples
+#' # Work with yaml configuration file:
+#' S7schema(
+#'   file = system.file("examples/config.yml", package = "S7schema"),
+#'   schema = system.file("examples/schema.json", package = "S7schema")
+#' )
+#'
+#' # Create object in memory
+#' S7schema(
+#'   .data = list(my_config_var = 6),
+#'   schema = system.file("examples/schema.json", package = "S7schema")
+#' )
+#' @name S7schema
+NULL
+
 #' @noRd
 validate_prop_schema <- function(value) {
   val <- check_file(file = value, ext = "json")
@@ -78,51 +126,7 @@ validate_S7schema <- function(self) {
   )
 }
 
-#' Work with valid configurations
-#' @description
-#' `S7schema()` provides a generic way of working with yaml configuration files.
-#'
-#' The object is created by supplying both an initial YAML configuration (`file`)
-#' and the JSON schema definition (`schema`) of the configuration file.
-#'
-#' The initial configuration is validated before the new object is returned.
-#' If not valid the first error is thrown, together with a path to the entry in the YAML file,
-#' and a description of the error.
-#'
-#' The `S7schema` class inherits from `list`, ensuring that the content of the YAML
-#' file can be accessed as if read directly with `yaml::read_yaml()`, and supports
-#' the below workflow:
-
-#' 1. Read and validate config file: `x <- S7schema(...)`
-#' 2. Edit content as if it was a list: `x$new_entry <- "new_value"`
-#' 3. Validate new content against the original schema: `validate(x)`
-#' 4. Use values in downstream functions: `x$new_entry`
-#'
-#' @details
-#' See internal [validator()] documentation for more info on how the validation is done.
-#'
-#' @param file `character(1)` path to a yaml file to be checked.
-#' @param schema `character(1)` path to a JSON schema.
-#' @param .data `list()` input to create the object. Mutually exclusive with `file`.
-#' @section Properties:
-#' \describe{
-#'   \item{schema}{`character(1)` path to JSON schema being used to validate against.}
-#'   \item{validator}{Internal [validator()] used to validate the content (read-only).}
-#'   \item{file}{`character(1)` path to the source YAML file.}
-#' }
-#' @returns New `S7schema` object.
-#' @examples
-#' # Work with yaml configuration file:
-#' S7schema(
-#'   file = system.file("examples/config.yml", package = "S7schema"),
-#'   schema = system.file("examples/schema.json", package = "S7schema")
-#' )
-#'
-#' # Create object in memory
-#' S7schema(
-#'   .data = list(my_config_var = 6),
-#'   schema = system.file("examples/schema.json", package = "S7schema")
-#' )
+#' @rdname S7schema
 #' @export
 S7schema <- S7::new_class(
   name = "S7schema",
